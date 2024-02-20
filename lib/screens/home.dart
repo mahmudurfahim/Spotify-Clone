119f6574da9036ccc75a524b5e095f5c058477b4
Fahim <mahmudurrahmanfampoo@gmail.com>
2024-02-21 03:37:13 +0600

    Initial Commit

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -3,12 +3,42 @@ import 'package:flutter/material.dart';
 class Home extends StatelessWidget {
   const Home({Key? key}) : super(key: key);
 
+  createAppBar(String message){
+    return AppBar(
+      backgroundColor: Colors.transparent,
+      elevation: 0.0,
+      title: Text(message,style: const TextStyle(
+        color: Colors.black)),
+
+      actions: const [Padding(
+        padding: EdgeInsets.only(right: 10),
+
+          child: Icon(Icons.settings))],
+
+    );
+
+  }
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
 
         child:Container(
-          color: Colors.orange,
+
+          decoration: BoxDecoration(
+            gradient: LinearGradient(colors: [
+              Colors.blueGrey.shade300,
+              Colors.black],
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+                stops:const [0.1, 0.3]
+            )
+          ),
+
+          child: Column(children: [
+            createAppBar('Good morning')
+
+          ],),
 
         )
 
